04b37439c3bcaf18aaecb74e9d29261053502be0
crypto_mrx <maksim.ilonov@yandex.ru>
2025-08-18 16:34:38 +0300

    add auto tmux in tmux.nix and zsh.nix. Fix bugs in monitors after reboot.

--- NixOs/Huawei/home-manager/modules/hyprland/main.nix
+++ NixOs/Huawei/home-manager/modules/hyprland/main.nix
@@ -13,7 +13,7 @@
         "XDG_SCREENSHOTS_DIR,$HOME/screens"
       ];
 
-      monitor = ",1920x1080@60,auto,1.1";
+      monitor = ",1920x1080@60,auto,1.25";
       "$mainMod" = "SUPER";
       "$terminal" = "alacritty";
       "$fileManager" = "$terminal -e sh -c 'ranger'";
