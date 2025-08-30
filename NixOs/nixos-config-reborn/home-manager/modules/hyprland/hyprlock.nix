8419b05a3058b2e53b547d793aa0fca2a44cdbf2
crypto_mrx <maksim.ilonov@yandex.ru>
2025-08-30 22:21:23 +0300

    work on hyprlock

--- NixOs/nixos-config-reborn/home-manager/modules/hyprland/hyprlock.nix
+++ NixOs/nixos-config-reborn/home-manager/modules/hyprland/hyprlock.nix
@@ -12,8 +12,9 @@
 # Настройки фона
         background = [
         {
-# Делаем скриншот текущего рабочего стола и затемняем его
+# Устанавливаем изображение
           path = "/home/admsys/wallpapers/otherWallpaper/gruvbox/forest_road.jpg";
+# Применяем эффекты
           blur_passes = 4;
           blur_size = 10;
           contrast = 0.5;
@@ -22,18 +23,46 @@
         }
         ];
 
-# Поле для ввода пароля
+# Настройки для текстовых элементов (Time и Date)
+        label = [
+# Время
+        {
+          monitor = "";
+          text = ''cmd[update:1000] echo "$(date +'%k:%M')"'';
+          color = "rgba(235, 219, 178, 0.9)";
+          font_size = 115;
+          font_family = "Maple Mono Bold";
+          shadow_passes = 3;
+          position = "0, -25";
+          halign = "center";
+          valign = "top";
+        }
+# Дата
+        {
+          monitor = "";
+          text = ''cmd[update:1000] echo "- $(date +'%A, %B %d') -" '';
+          color = "rgba(235, 219, 178, 0.9)";
+          font_size = 18;
+          font_family = "Maple Mono";
+          shadow_passes = 3;
+          position = "0, -25";
+          halign = "center";
+          valign = "top";
+        }
+        ];
+
+# Поле для ввода пароля (отдельный элемент)
         input-field = [
         {
 # Позиция поля ввода
           position = "0, 0";
 # Размер
           size = "300, 60";
-# Цвет текста (зеленый)
+# Цвет текста
           font_color = "rgb(00ff00)";
-# Цвет внутренней части поля (темный)
+# Цвет внутренней части
           inner_color = "rgb(1a1a1a)";
-# Цвет границы (зеленый)
+# Цвет границы
           outer_color = "rgb(00ff00)";
 # Толщина границы
           outline_thickness = 2;
