fd5cdcdfee3a16221d758d98fb51f9899df912a2
crypto_mrx <maksim.ilonov@yandex.ru>
2025-09-04 14:54:41 +0300

    add border colour and opacity hyprland

--- NixOs/nixos-config-reborn/home-manager/modules/hyprland/main.nix
+++ NixOs/nixos-config-reborn/home-manager/modules/hyprland/main.nix
@@ -4,13 +4,13 @@
     systemd.enable = true;
     settings = {
       env = [
-        # Hint Electron apps to use Wayland
+# Hint Electron apps to use Wayland
         "NIXOS_OZONE_WL,1"
-        "XDG_CURRENT_DESKTOP,Hyprland"
-        "XDG_SESSION_TYPE,wayland"
-        "XDG_SESSION_DESKTOP,Hyprland"
-        "QT_QPA_PLATFORM,wayland"
-        "XDG_SCREENSHOTS_DIR,$HOME/screens"
+          "XDG_CURRENT_DESKTOP,Hyprland"
+          "XDG_SESSION_TYPE,wayland"
+          "XDG_SESSION_DESKTOP,Hyprland"
+          "QT_QPA_PLATFORM,wayland"
+          "XDG_SCREENSHOTS_DIR,$HOME/screens"
       ];
 
       monitor = ",1920x1080@60,auto,1.25";
@@ -21,8 +21,8 @@
 
       exec-once = [
         "waybar"
-        "wl-paste --type text --watch cliphist store"
-        "wl-paste --type image --watch cliphist store"
+          "wl-paste --type text --watch cliphist store"
+          "wl-paste --type image --watch cliphist store"
       ];
 
       general = {
@@ -31,7 +31,7 @@
 
         border_size = 5;
 
-        "col.active_border" = "rgba(d65d0eff) rgba(98971aff) 45deg";
+        "col.active_border" = "rgba(89b4faff) rgba(f5c2e7ff) 45deg";
         "col.inactive_border" = "rgba(3c3836ff)";
 
         resize_on_border = true;
@@ -41,10 +41,10 @@
       };
 
       decoration = {
-        rounding = 0;
+        rounding = 10;
 
-        active_opacity = 1.0;
-        inactive_opacity = 1.0;
+        active_opacity = 0.85;
+        inactive_opacity = 0.85;
 
         shadow = {
           enabled = false;
@@ -67,7 +67,7 @@
       gestures = {
         workspace_swipe = true;
         workspace_swipe_invert = false;
-        workspace_swipe_forever	= true;
+        workspace_swipe_forever = true;
       };
 
       dwindle = {
@@ -89,31 +89,31 @@
       windowrulev2 = [
         "bordersize 0, floating:0, onworkspace:w[t1]"
 
-        "float,class:(mpv)|(imv)|(showmethekey-gtk)"
-        "move 990 60,size 900 170,pin,noinitialfocus,class:(showmethekey-gtk)"
-        "noborder,nofocus,class:(showmethekey-gtk)"
+          "float,class:(mpv)|(imv)|(showmethekey-gtk)"
+          "move 990 60,size 900 170,pin,noinitialfocus,class:(showmethekey-gtk)"
+# "noborder,nofocus,class:(showmethekey-gtk)"
 
-        # "workspace 3,class:(obsidian)"
-        # "workspace 3,class:(zathura)"
-        # "workspace 4,class:(com.obsproject.Studio)"
-        # "workspace 5,class:(telegram)"
-        # "workspace 5,class:(vesktop)"
-        # "workspace 6,class:(teams-for-linux)"
+# "workspace 3,class:(obsidian)"
+# "workspace 3,class:(zathura)"
+# "workspace 4,class:(com.obsproject.Studio)"
+# "workspace 5,class:(telegram)"
+# "workspace 5,class:(vesktop)"
+# "workspace 6,class:(teams-for-linux)"
 
-        "suppressevent maximize, class:.*"
-        "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
+          "suppressevent maximize, class:.*"
+          "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
 
-        "opacity 0.0 override, class:^(xwaylandvideobridge)$"
-        "noanim, class:^(xwaylandvideobridge)$"
-        "noinitialfocus, class:^(xwaylandvideobridge)$"
-        "maxsize 1 1, class:^(xwaylandvideobridge)$"
-        "noblur, class:^(xwaylandvideobridge)$"
-        "nofocus, class:^(xwaylandvideobridge)$"
-      ];
+          "opacity 0.0 override, class:^(xwaylandvideobridge)$"
+          "noanim, class:^(xwaylandvideobridge)$"
+          "noinitialfocus, class:^(xwaylandvideobridge)$"
+          "maxsize 1 1, class:^(xwaylandvideobridge)$"
+          "noblur, class:^(xwaylandvideobridge)$"
+          "nofocus, class:^(xwaylandvideobridge)$"
+          ];
 
       workspace = [
         "w[tv1], gapsout:0, gapsin:0"
-        "f[1], gapsout:0, gapsin:0"
+          "f[1], gapsout:0, gapsin:0"
       ];
     };
   };
