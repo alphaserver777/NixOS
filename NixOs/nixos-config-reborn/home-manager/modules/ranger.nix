52e624d21cf2912b4590e86dcf1f3cd730a67ead
crypto_mrx <maksim.ilonov@yandex.ru>
2025-09-04 14:37:58 +0300

    wofi - change style and ranger add previe pictures

--- NixOs/nixos-config-reborn/home-manager/modules/ranger.nix
+++ NixOs/nixos-config-reborn/home-manager/modules/ranger.nix
@@ -13,8 +13,8 @@
     };
 
     settings = {
-# preview_images = true;
-# preview_images_method = "ueberzug";
+      preview_images = true;
+      preview_images_method = "ueberzug";
       draw_borders = true;
       w3m_delay = 0;
     };
