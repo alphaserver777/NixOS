cf7edce5239fa063f91597630f65058b882e3808
crypto_mrx <maksim.ilonov@yandex.ru>
2025-09-04 19:15:21 +0300

    add progs for srv-home

--- NixOs/nixos-config-reborn/hosts/srv-home/local-packages.nix
+++ NixOs/nixos-config-reborn/hosts/srv-home/local-packages.nix
@@ -1,23 +1,36 @@
 { pkgs, ... }: {
-  # Частные программы узла
+# Частные программы узла
   environment.systemPackages = with pkgs; [
     google-chrome
-    btop
-    telegram-desktop
-    # obsidian
-    syncthing
-    amnezia-vpn
-    # keepass
-    # qbittorrent
-    home-manager
-    neovim
-    docker
-    docker-compose
-    # gcc
-    # kdenlive
-    # jetbrains.pycharm-professional
-    # jre8
-    # qemu
-    # quickemu
-  ];
-}
+      btop
+      telegram-desktop
+# obsidian
+      syncthing
+      amnezia-vpn
+# keepass
+# qbittorrent
+      home-manager
+      imv #pic
+      mpv #video
+#CLI
+      btop
+      fzf # fast file search
+      git-graph # visual for git
+      ntfs3g # driver for NTFS
+      ripgrep # fast text search
+      udisks # for USB
+      ueberzugpp # pic in terminal
+      w3m # web-browser in terminal
+      zip
+      unzip
+      neovim
+      docker
+      docker-compose
+# gcc
+# kdenlive
+# jetbrains.pycharm-professional
+# jre8
+# qemu
+# quickemu
+      ];
+               }
