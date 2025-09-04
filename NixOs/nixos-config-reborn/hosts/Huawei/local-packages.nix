84865fc87ac0492b4cfbe36d98cfd833f7ab8149
crypto_mrx <maksim.ilonov@yandex.ru>
2025-09-04 22:20:31 +0300

    add some progs for Huawei

--- NixOs/nixos-config-reborn/hosts/Huawei/local-packages.nix
+++ NixOs/nixos-config-reborn/hosts/Huawei/local-packages.nix
@@ -2,12 +2,28 @@
 # Частные программы узла
   environment.systemPackages = with pkgs; [
     google-chrome
-      btop
+      zed-editor
       telegram-desktop
       obsidian
       syncthing
       amnezia-vpn
+      keepassxc
       qbittorrent
+      imv #pic
+      mpv #video
+
+#CLI
+      btop
+      fzf # fast file search
+      git-graph # visual for git
+      ntfs3g # driver for NTFS
+      ripgrep # fast text search
+      udisks # for USB
+      ueberzugpp # pic in terminal
+      w3m # web-browser in terminal
+      zip
+      unzip
+      p7zip
       home-manager
       neovim
 # gcc
@@ -16,5 +32,5 @@
 # jre8
 # qemu
 # quickemu
-  ];
+      ];
                }
