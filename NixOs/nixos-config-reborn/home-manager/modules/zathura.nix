ec06bbf9d400244bfee4e11bb94ae45fe30a64e9
crypto_mrx <maksim.ilonov@yandex.ru>
2025-09-07 21:11:11 +0300

    add copy text in zathura

--- NixOs/nixos-config-reborn/home-manager/modules/zathura.nix
+++ NixOs/nixos-config-reborn/home-manager/modules/zathura.nix
@@ -5,9 +5,11 @@
       D = "toggle_page_mode";
       d = "scroll half_down";
       u = "scroll half_up";
+      "<C-y>" = "exec echo $FILE | wl-copy";
     };
     options = {
       font = "JetBrains Mono Bold 13";
+      selection-clipboard = "clipboard";
     };
   };
 }
