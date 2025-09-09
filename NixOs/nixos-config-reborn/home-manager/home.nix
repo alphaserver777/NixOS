e6096570c9beeb401e2bd0669a39aa3379957ff1
crypto_mrx <maksim.ilonov@yandex.ru>
2025-09-09 15:46:44 +0300

    feat: Добавлена переменная сессии для Gemini-cli
    Добавлена переменная окружения GOOGLE_CLOUD_PROJECT в home.nix для
    корректной работы утилиты Gemini-cli. Это позволяет пользователю
    взаимодействовать с проектом Google Cloud "nixos-ai-001" напрямую из
    терминала.

--- NixOs/nixos-config-reborn/home-manager/home.nix
+++ NixOs/nixos-config-reborn/home-manager/home.nix
@@ -1,12 +1,16 @@
 { homeStateVersion, user, ... }: {
   imports = [
     ./modules
-    ./home-packages.nix
+      ./home-packages.nix
   ];
 
   home = {
     username = user;
     homeDirectory = "/home/${user}";
     stateVersion = homeStateVersion;
+
+    sessionVariables = { # Переменная для работы Gemini-cli
+      GOOGLE_CLOUD_PROJECT = "nixos-ai-001";
+    };
   };
-}
+                                 }
