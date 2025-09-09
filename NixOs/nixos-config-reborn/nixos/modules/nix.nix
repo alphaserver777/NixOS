85ab184a4dc6532ce526ddf5e02b001220facb64
crypto_mrx <maksim.ilonov@yandex.ru>
2025-09-09 16:50:55 +0300

    refactor: Исправлены права доступа для Syncthing
    Это изменение удаляет устаревшие и неэффективные методы
    (`users.users.syncthing` и `systemd.tmpfiles.rules`) и заменяет их на
    более надёжный `postStart` скрипт. Скрипт выполняется сразу после
    запуска службы Syncthing и принудительно устанавливает права доступа
    `775` для каталога данных `/var/lib/syncthing`, что решает проблему,
    когда Syncthing автоматически сбрасывает права на `700`.

--- NixOs/nixos-config-reborn/nixos/modules/nix.nix
+++ NixOs/nixos-config-reborn/nixos/modules/nix.nix
@@ -1,4 +1,15 @@
 {
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix = {
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+      auto-optimise-store = true;
+    };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
+  };
+
   nixpkgs.config.allowUnfree = true;
 }
