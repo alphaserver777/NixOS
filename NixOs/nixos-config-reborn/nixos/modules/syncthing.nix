e6e304d2fba008163a58943f5cd1228192b46448
crypto_mrx <maksim.ilonov@yandex.ru>
2025-09-10 10:30:00 +0300

    fix: Изменён пользователь и путь для Syncthing
    Это изменение исправляет конфигурацию Syncthing, переназначая
    пользователя и пути к файлам на текущего пользователя (`admsys`). Теперь
    `dataDir` и `configDir` указывают на домашнюю директорию пользователя,
    что решает проблемы с правами доступа, а также делает Syncthing более
    подходящим для управления пользовательскими данными.

--- NixOs/nixos-config-reborn/nixos/modules/syncthing.nix
+++ NixOs/nixos-config-reborn/nixos/modules/syncthing.nix
@@ -1,18 +1,10 @@
 {
   services.syncthing = {
     enable = true;
-    user = "syncthing";
-    group = "syncthing";
-    dataDir = "/var/lib/syncthing";
-    configDir = "/var/lib/syncthing/.config/syncthing";
+    user = "admsys";
+    group = "users";
+    dataDir = "/home/admsys/Obsidian";
+    configDir = "/home/admsys/.config/syncthing";
   };
-
-# journalctl -u syncthing
-# systemctl status syncthing
-# Этот скрипт запускается после старта службы syncthing,
-# чтобы установить правильные права доступа к каталогу данных.
-# Syncthing при запуске сбрасывает права на 700, поэтому это необходимо.
-  systemd.services.syncthing.postStart = ''
-    /run/current-system/sw/bin/chmod -R 775 /var/lib/syncthing
-    '';
 }
+
