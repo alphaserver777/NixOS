02b7a7f3880c812bce0898d8544b8eb5e01234e7
crypto_mrx <maksim.ilonov@yandex.ru>
2025-09-14 22:20:12 +0300

    fix: Гугл сменил на firefox

--- NixOs/nixos-config-reborn/hosts/srv-home/local-packages.nix
+++ NixOs/nixos-config-reborn/hosts/srv-home/local-packages.nix
@@ -1,40 +1,35 @@
 { pkgs, ... }: {
 # Частные программы узла
   environment.systemPackages = with pkgs; [
-    (google-chrome.override {
-     commandLineArgs = [
-     "--enable-features=UseOzonePlatform,WaylandWindowDecorations"
-     "--ozone-platform=wayland"
-     ];
-     })
-  btop
-    telegram-desktop
+    firefox
+      btop
+      telegram-desktop
 # obsidian
-    syncthing
-    amnezia-vpn
+      syncthing
+      amnezia-vpn
 # keepass
 # qbittorrent
-    home-manager
-    imv #pic
-    mpv #video
+      home-manager
+      imv #pic
+      mpv #video
 #CLI
-    btop
-    git-graph # visual for git
-    ntfs3g # driver for NTFS
-    ripgrep # fast text search
-    udisks # for USB
-    ueberzugpp # pic in terminal
-    w3m # web-browser in terminal
-    zip
-    unzip
-    neovim
-    docker
-    docker-compose
+      btop
+      git-graph # visual for git
+      ntfs3g # driver for NTFS
+      ripgrep # fast text search
+      udisks # for USB
+      ueberzugpp # pic in terminal
+      w3m # web-browser in terminal
+      zip
+      unzip
+      neovim
+      docker
+      docker-compose
 # gcc
 # kdenlive
 # jetbrains.pycharm-professional
 # jre8
 # qemu
 # quickemu
-    ];
+      ];
                }
