9f6da702e373f06bc8a5fa5e5f05bfb4bacb22c0
crypto_mrx <maksim.ilonov@yandex.ru>
2025-09-15 08:27:35 +0300

    fix: Исправлен звук на ноутбуке

--- NixOs/nixos-config-reborn/hosts/Huawei/configuration.nix
+++ NixOs/nixos-config-reborn/hosts/Huawei/configuration.nix
@@ -7,10 +7,25 @@
     ../../nixos/modules
   ];
 
+  hardware.enableAllFirmware = true;
+
   environment.systemPackages = [ pkgs.home-manager ];
 
   networking.hostName = hostname;
 
   system.stateVersion = stateVersion;
+
+  systemd.services.fix-sound-essx8336 = {
+    description = "Set ALSA mixer controls for sof-essx8336";
+    after = [ "sound.target" ];
+    wantedBy = [ "multi-user.target" ];
+    script = ''
+      ${pkgs.alsa-utils}/bin/amixer -c 0 cset name='Left Headphone Mixer Left DAC Switch' on
+      ${pkgs.alsa-utils}/bin/amixer -c 0 cset name='Right Headphone Mixer Right DAC Switch' on
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+    };
+  };
 }
 
