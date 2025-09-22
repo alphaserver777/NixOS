88d1e5e313a74b976ecb204858e80a2e2d99a7fe
crypto_mrx <maksim.ilonov@yandex.ru>
2025-09-22 10:42:34 +0300

    style: Форматирование кода и добавление автоматической очистки в Git конфигурацию
    Это изменение вносит косметические улучшения в форматирование кода и
    добавляет полезную опцию автоматической очистки удалённых веток.
    
    ### Изменения в `git.nix`
    
    *   **Форматирование кода:** Исправлены отступы с табуляции на пробелы
    для соответствия стилю Nix
    *   **Автоматическая очистка:** Добавлена опция `fetch.prune = true` для
    автоматического удаления ссылок на удалённые ветки при выполнении `git
    fetch`
    
    ### Преимущества изменений
    
    *   **Единообразие кода:** Приведение форматирования в соответствие с
    общепринятыми стандартами Nix
    *   **Автоматическое обслуживание:** Git будет автоматически удалять
    ссылки на удалённые ветки, которые больше не существуют на сервере
    *   **Сокращение мусора:** Уменьшение размера локального репозитория за
    счёт автоматической очистки устаревших ссылок

--- nixos-config/home-manager/modules/git.nix
+++ nixos-config/home-manager/modules/git.nix
@@ -1,10 +1,11 @@
 {
-	programs.git = {
-		enable = true;
-		userName = "crypto_mrx";
-		userEmail = "maksim.ilonov@yandex.ru";
-		extraConfig = {
-			url."git@github.com:".insteadOf = "https://github.com/";
-		};
-	};
+  programs.git = {
+    enable = true;
+    userName = "crypto_mrx";
+    userEmail = "maksim.ilonov@yandex.ru";
+    extraConfig = {
+      url."git@github.com:".insteadOf = "https://github.com/";
+      fetch.prune = true;
+    };
+  };
 }
