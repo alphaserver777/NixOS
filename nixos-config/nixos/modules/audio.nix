884eb5551a20298720a1b461cf1950488ab78b39
crypto_mrx <maksim.ilonov@yandex.ru>
2025-10-16 22:57:04 +0300

    feat: Переход на современный звуковой стек PipeWire с полной функциональностью
    Это изменение полностью переводит звуковую подсистему на современный
    стек PipeWire, заменяя устаревший PulseAudio и добавляя поддержку
    профессиональных аудио-инструментов.
    
    ### Основные изменения
    
    *   **Отключение PulseAudio:** Явное отключение
    `hardware.pulseaudio.enable = false`
    *   **Полная активация PipeWire:** Включение всех основных компонентов
    современного звукового стека
    
    ### Расширенная конфигурация PipeWire
    
    *   **ALSA совместимость:** Поддержка legacy-приложений через
    `alsa.enable = true` и `alsa.support32Bit = true`
    *   **PulseAudio эмуляция:** Совместимость с приложениями,
    разработанными для PulseAudio через `pulse.enable = true`
    *   **Профессиональный аудио:** Поддержка JACK (`jack.enable = true`)
    для профессиональных аудио-приложений и DAW
    *   **Современный менеджме

--- nixos-config/nixos/modules/audio.nix
+++ nixos-config/nixos/modules/audio.nix
@@ -1,10 +1,13 @@
 {
   security.rtkit.enable = true;
+  hardware.pulseaudio.enable = false;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    jack.enable = true;
+    wireplumber.enable = true;
   };
   hardware.alsa.enablePersistence = true;
 }
