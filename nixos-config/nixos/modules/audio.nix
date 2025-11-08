61ae24325395376d789e1f8a8c6bc0fff830389e
crypto_mrx <maksim.ilonov@yandex.ru>
2025-11-08 10:47:29 +0300

    fix: Пофиксил баг на звуке Huawei

--- nixos-config/nixos/modules/audio.nix
+++ nixos-config/nixos/modules/audio.nix
@@ -7,7 +7,37 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
-    wireplumber.enable = true;
+    extraConfig.pipewire."20-disable-x11-bell.conf" = {
+      "context.modules" = [
+        {
+          name = "libpipewire-module-x11-bell";
+          flags = [ "ifexists" "nofail" ];
+          enable = false;
+        }
+      ];
+    };
+    wireplumber = {
+      enable = true;
+      extraConfig = {
+        "10-disable-suspension" = {
+          "wireplumber.settings" = {
+            "monitor.alsa" = {
+              "properties" = {
+                "session.suspend-timeout-seconds" = 0;
+              };
+            };
+          };
+        };
+      };
+    };
   };
   hardware.alsa.enablePersistence = true;
+  services.power-profiles-daemon.enable = false;
+  services.tlp = {
+    enable = true;
+    extraConfig = ''
+      SOUND_POWER_SAVE_ON_BAT=0
+      SOUND_POWER_SAVE_ON_AC=0
+    '';
+  };
 }
