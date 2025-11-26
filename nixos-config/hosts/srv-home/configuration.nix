f89b16be087fa573f237e58c4dfe33d195d5dd81
crypto_mrx <maksim.ilonov@yandex.ru>
2025-11-26 20:59:33 +0300

    fix: Улучшение стабильности WiFi подключения
    - Увеличено время ожидания устройства WiFi до 30 секунд через
    `networking.networkmanager.wait-for-device-timeout = 30`
    - Добавлена опция `options iwlwifi power_save=0` для отключения
    энергосбережения WiFi

--- nixos-config/hosts/srv-home/configuration.nix
+++ nixos-config/hosts/srv-home/configuration.nix
@@ -13,6 +13,9 @@
   # Disable WiFi power saving to prevent intermittent connection drops
   boot.extraModprobeConfig = "options iwlwifi power_save=0";
 
+  # Wait for the WiFi device to be ready before attempting to connect.
+  networking.networkmanager.wait-for-device-timeout = 30;
+
   environment.systemPackages = [ pkgs.home-manager ];
 
   networking.hostName = hostname;
